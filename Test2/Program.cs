639534b6ca5e1ec463fe713bf2d0e7a8029769d4
AliGuseinov <rrrerssrrrrr@bk.ru>
2018-03-05 22:46:09 +0300

    Fib

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -6,7 +6,24 @@ namespace Test2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+                int n;
+                Console.WriteLine("Введите значение n");
+                n = int.Parse(Console.ReadLine());
+                int[] numbers = new int[n];
+                numbers[0] = 0;
+                numbers[1] = 1;
+                for (int i = 2; i < numbers.Length; i++)
+                {
+                    numbers[i] = numbers[i - 1] + numbers[i - 2];
+                }
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.WriteLine(numbers);
+                }
+
+
+            
         }
     }
 }
