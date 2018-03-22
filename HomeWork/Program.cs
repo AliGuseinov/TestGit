388ac1c7b1e66f5852146011614e9fd5d5ffa4f8
AliGuseinov <rrrerssrrrrr@bk.ru>
2018-03-22 20:12:28 +0300

    Array

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,54 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HomeWork
 {
     class Station
     {
-        public string Name
-        {
-            get;
-            set;
-        }
-        public string Adress
-        {
-            get;
-            set;
+        //наименование
+        public string Name { get; private set; }
+
+        public Station(string name){
+            Name = name;
         }
     }
 
     class Route
     {
-        public int BusNumber
-        {
-            get;
-            set;
+        //станции
+        public List<Station> Stations { get; set; }
+
+        //время начала движения
+        public DateTime TimeStart { get; private set; }
+        //время конца движения
+        public DateTime TimeEnd { get; private set; }
+        //интервал движения
+        public TimeSpan Interval { get; private set; }
+
+        public Route(DateTime timeStart, DateTime timeEnd, TimeSpan interval){
+            TimeStart = timeStart;
+            TimeEnd = timeEnd;
+            Interval = interval;
+
+            Stations = new List<Station>();
         }
-        public string NextStationName
-        {
-            get;
-            set;
+
+        public Route(DateTime timeStart, DateTime timeEnd, TimeSpan interval, List<Station> stations){
+            TimeStart = timeStart;
+            TimeEnd = timeEnd;
+            Interval = interval;
+
+            Stations = stations;
         }
     }
 
-    class StationsGraph
-    {
+    //class StationsGraph
+    //{
         
-    }
+    //}
 
-    class ScheduleSearcher
-    {
-        public int NearestBus
-        {
-            get;
-            set;
-        }
-    }
+    //class ScheduleSearcher
+    //{
+        
+    //}
 
     class Program
     {
         static void Main(string[] args)
         {
-       
+            Station station = new Station("белорусская");
+            List<Station> stations = new List<Station>();
+            stations.Add(station);
+
+            Route route1 = new Route(
+                new DateTime(2018, 3, 12, 8, 0, 0),
+                new DateTime(2018, 3, 12, 23, 0, 0),
+                new TimeSpan(0, 10, 0)
+            );
+            route1.Stations.Add(station);
+
+            Route route2 = new Route(
+                new DateTime(2018, 3, 12, 8, 0, 0),
+                new DateTime(2018, 3, 12, 23, 0, 0),
+                new TimeSpan(0, 10, 0),
+                stations
+            );
+
+            Console.WriteLine(station);
+            Console.WriteLine(route1);
         }
     }
 }
