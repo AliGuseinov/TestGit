125914207def949d9329d6ea63b78fc8a7e999a1
AliGuseinov <rrrerssrrrrr@bk.ru>
2018-03-28 21:58:04 +0300

    HM2

---
+++ HW2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] array = new int[3][];
+            Random rnd = new Random();
+
+            int i = 0;
+            int j = 0;
+
+            while (i < array.Length) 
+            {
+                array[i] = new int[3];
+                j = 0;
+                while (j < array[i].Length) 
+                {
+                    array[i][j] = rnd.Next();
+
+                        Console.WriteLine(array[i][j]);
+                    j++;
+                }
+                i++;
+            }
+        }
+    }
+}
