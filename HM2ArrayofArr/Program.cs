5edc069cb2be1829244b8dc45032efb61b0ed4f7
AliGuseinov <rrrerssrrrrr@bk.ru>
2018-03-30 01:54:59 +0300

    HM2ArrayofArr

---
+++ HM2ArrayofArr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HM2ArrayofArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] array = new int[10][];
+            Random rnd = new Random();
+            int i = 0;
+            int j = 0;
+            while (i < array.Length)
+            {
+                array[i] = new int[10];
+                j = 0;
+                while (j < array[i].Length)
+                {
+                    array[i][j] = rnd.Next();
+                    Console.WriteLine(array[i][j]);
+                    j++;
+                }
+                i++;
+            }
+        }
+    }
+}
